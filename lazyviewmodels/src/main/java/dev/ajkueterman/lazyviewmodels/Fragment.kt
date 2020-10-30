69104cc515a9f34b0f2bbd2b87a0da7599a6a48a
AJ Kueterman <ajkueterman@gmail.com>
2020-10-30 10:24:09 -0400

    Enable extensions to get a SavedStateHandle for your ViewModels

--- lazyviewmodels/src/main/java/dev/ajkueterman/lazyviewmodels/Fragment.kt
+++ lazyviewmodels/src/main/java/dev/ajkueterman/lazyviewmodels/Fragment.kt
@@ -1,10 +1,10 @@
 package dev.ajkueterman.lazyviewmodels
 
+import androidx.activity.ComponentActivity
 import androidx.annotation.MainThread
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelLazy
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
+import androidx.savedstate.SavedStateRegistryOwner
 
 /**
  * An extension on [Fragment] that provides a [ViewModel] by calling [activityViewModelBuilder]
@@ -18,7 +18,7 @@ import androidx.lifecycle.ViewModelProvider
  * @return a [VM] class instantiated by [Lazy], scoped to the Activity
  */
 @MainThread
-inline fun <reified VM : ViewModel> Fragment.activityViewModelBuilder(
+inline fun <reified VM : ViewModel> Fragment.lazyActivityViewModels(
     noinline viewModelInitializer: () -> VM
 ): Lazy<VM> =
     ViewModelLazy(
@@ -28,8 +28,43 @@ inline fun <reified VM : ViewModel> Fragment.activityViewModelBuilder(
             object : ViewModelProvider.Factory {
                 override fun <T : ViewModel?> create(modelClass: Class<T>): T {
                     @Suppress("UNCHECKED_CAST")// Casting T as ViewModel
-                    return viewModelInitializer.invoke() as T
+                    return viewModelInitializer() as T
                 }
+            }
+        }
+    )
+
+/**
+ * An extension for [Fragment] that provides a [ViewModel] by calling
+ * [viewModelInitializer] with a lambda closure that provides a [SavedStateHandle] and returns your
+ * custom [VM] class [ViewModel]. Returns a lazily instantiated [ViewModel] of your provided type
+ * using an [AbstractSavedStateViewModelFactory].
+ *
+ * Like `by activityViewModels()`, this extension creates a [ViewModel] scoped to the parent
+ * [ComponentActivity].
+ *
+ * @param owner the [SavedStateRegistryOwner] used to provide the [SavedStateHandle], defaults
+ * to this [Fragment]
+ * @param viewModelInitializer the lambda which returns your custom class [VM] of type [ViewModel]
+ *
+ * @return a [VM] class instantiated by [Lazy], scoped to the Activity
+ */
+@MainThread
+inline fun <reified VM : ViewModel> Fragment.lazySavedStateActivityViewModels(
+    owner: SavedStateRegistryOwner = this,
+    noinline viewModelInitializer: (SavedStateHandle) -> VM
+): Lazy<VM> =
+    ViewModelLazy(
+        viewModelClass = VM::class,
+        storeProducer = { requireActivity().viewModelStore },
+        factoryProducer = {
+            return@ViewModelLazy object : AbstractSavedStateViewModelFactory(owner, null) {
+                @Suppress("UNCHECKED_CAST")// Casting T as ViewModel
+                override fun <T : ViewModel?> create(
+                    key: String,
+                    modelClass: Class<T>,
+                    handle: SavedStateHandle
+                ): T = viewModelInitializer(handle) as T
             }
         }
     )
